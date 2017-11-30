ee2a342e9cb9aec58ad0a02e05b5fb8cee7516d9
Scottie Enriquez <scottie@enriquez.io>
2017-11-29 21:21:42 -0600

    Successfully fetched playlist

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var args = process.argv.slice(2);
 var SpotifyWebApi = require('spotify-web-api-node');
-var spotifyApi = new SpotifyWebApi({
-    clientId : args[0],
-    clientSecret : args[1]
-});
 
+var args = process.argv.slice(2);
+var spotifyClientId = args[0];
+var spotifyClientSecret = args[1];
+var spotifyTargetUsername = args[2];
+var spotifyTargetPlaylistId = args[3];
+
+var spotifyApi = new SpotifyWebApi({
+    clientId : spotifyClientId,
+    clientSecret : spotifyClientSecret
+});
 spotifyApi.clientCredentialsGrant()
-    .then(function(data) {
-        spotifyApi.setAccessToken(data.body['access_token']);
-    }, function(error) {
-        console.log('Something went wrong when retrieving a Spotify access token', error);
+    .then(function(clientCredentialData) {
+        spotifyApi.setAccessToken(clientCredentialData.body['access_token']);
+
+        spotifyApi.getPlaylist(spotifyTargetUsername, spotifyTargetPlaylistId)
+            .then(function(playlistData) {
+                console.log(playlistData.body);
+            }, function(playlistError) {
+                console.log('Something went wrong when fetching playlist', playlistError);
+            });
+    }, function(clientCredentialError) {
+        console.log('Something went wrong when retrieving a Spotify access token', clientCredentialError);
     });
