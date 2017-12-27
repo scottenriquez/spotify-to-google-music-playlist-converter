c4c8376f4b8390cf0b05ce106463ad0f7f5f513e
Scottie Enriquez <scottie@enriquez.io>
2017-12-26 20:51:32 -0600

    Added Google Play connection and playlist creation functionality

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+var PlayMusic = require('playmusic');
 
 var args = process.argv.slice(2);
 var spotifyClientId = args[0];
 var spotifyClientSecret = args[1];
 var spotifyTargetUsername = args[2];
 var spotifyTargetPlaylistId = args[3];
+var googlePlayEmailAddress = args[4];
+var googlePlayPassword = args[5];
+var newPlaylistName = args[6];
 
 var spotifyApi = new SpotifyWebApi({
     clientId : spotifyClientId,
@@ -13,10 +17,27 @@ var spotifyApi = new SpotifyWebApi({
 spotifyApi.clientCredentialsGrant()
     .then(function(clientCredentialData) {
         spotifyApi.setAccessToken(clientCredentialData.body['access_token']);
-
         spotifyApi.getPlaylist(spotifyTargetUsername, spotifyTargetPlaylistId)
             .then(function(playlistData) {
-                console.log(playlistData.body);
+                var playMusicApi = new PlayMusic();
+                playMusicApi.init({
+                    email: googlePlayEmailAddress,
+                    password: googlePlayPassword
+                }, function(googlePlayConnectionError){
+                    if (googlePlayConnectionError){
+                        console.log('Something went wrong when connecting to Google Play', googlePlayConnectionError)
+                    }
+                    else {
+                        playMusicApi.addPlayList(newPlaylistName, function(googlePlayPlaylistCreationError){
+                            if (googlePlayConnectionError){
+                                console.log('Something went wrong when creating a new Google Play playlist', googlePlayPlaylistCreationError)
+                            }
+                            else {
+                                console.log('Playlist created');
+                            }
+                        });
+                    }
+                });
             }, function(playlistError) {
                 console.log('Something went wrong when fetching playlist', playlistError);
             });
